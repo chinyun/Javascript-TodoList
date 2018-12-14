9b71e0ce6f0e7625546cc45287a60e3767c810da
chinyunchen <chenclaire17@gmail.com>
2018-12-14 21:34:55 +0800

    modified

--- TodoList.js
+++ TodoList.js
@@ -2,17 +2,17 @@ var button1 = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-ul.onclick = function() {
-	var target = event.target;
-	target.classList.toggle("done");
-}
-
 function inputLength() {
 	return input.value.length;
 }
 
 function removeParent() {
 	event.target.parentNode.remove();
+}
+
+function beDone() {
+	event.target.parentNode.classList.toggle("done");
+	event.target.classList.toggle("done");
 }
 
 function createListElement() {
@@ -27,6 +27,12 @@ function createListElement() {
 	btn.appendChild(document.createTextNode("delete"));
 	li.appendChild(btn);
 	btn.onclick = removeParent;
+
+	var btn2 = document.createElement("button");
+	btn2.classList.toggle("btn2");
+	btn2.appendChild(document.createTextNode("done"));
+	li.appendChild(btn2);
+	btn2.onclick = beDone;
 }
 
 function addListAfterClick() {
